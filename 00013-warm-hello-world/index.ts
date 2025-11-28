1f605ba204015068352105d0a7ba0d69bda1cb72
naokihaba <naosprintrunner6385@gmail.com>
2025-11-29 03:08:17 +0900

    feat: add HelloWorld type challenge and utility types for testing

---
+++ 00013-warm-hello-world/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Hello, World!
+ *
+ * Type Challengesでは、型システム自体を使ってアサーション（検証）を行います。
+ *
+ * このチャレンジでは、以下のコードを変更してテストが通る（型チェックエラーがない）ようにしてください。
+ *
+ * ```ts
+ * // string型になることが期待されている
+ * type HelloWorld = any
+ *
+ * // これが動作するようにする必要がある
+ * type test = Expect<Equal<HelloWorld, string>>
+ * ```
+ */
+import { Equal, Expect } from "../utils"
+
+// 解答をここに記述してください
+type HelloWorld = string
+
+type test = Expect<Equal<HelloWorld, string>>
---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+// T が True であることを期待する
+export type Expect<T extends true> = T
+
+/**
+ * <Equal<A, B>>
+ * A・Bの厳密な比較をしている
+ * 関数型でラップすると分配を防げる
+ * ユニオン型を渡す場合（分配が起きる）
+ * type C = IsString<string | number>
+ * ↓ 内部でこう処理される
+ * IsString<string> | IsString<number>
+ * "YES" | "NO"
+ */
+export type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
